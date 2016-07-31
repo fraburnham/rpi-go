d8f9d3a5889a37c03dca2c8c60b6152266ffb499
Frank Burnham <fra.burnham@gmail.com>
2016-07-31 10:32:21 -0400

    Initial

---
+++ blinky-led/blink.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"rpi/gpio"
+	"time"
+)
+
+func timeoutGenerator(ctrlCh chan bool, timeoutCh chan bool, timeout time.Duration) {
+	fmt.Println("Starting timeoutGenerator")
+	for true {
+		select {
+		case <-ctrlCh:
+			fmt.Println("timeoutGenerator dying")
+			return
+		default:
+			time.Sleep(timeout)
+			timeoutCh <- true
+		}
+	}
+}
+
+func blink(ctrlCh chan bool, timeoutCh chan bool, pin *gpio.RpiGPIO) {
+	fmt.Println("Starting blink")
+	offset := 0
+
+	for true {
+		select {
+		case <-ctrlCh:
+			fmt.Println("blink dying")
+			return
+		case <-timeoutCh:
+			offset++
+			err := pin.WriteValue(offset % 2)
+			if err != nil {
+				panic(err.Error())
+			}
+		}
+	}
+
+}
+
+func main() {
+	ctrlCh := make(chan bool) //oh snap, I bet channels need to be closed
+	timeoutCh := make(chan bool)
+	timeout := time.Duration(1 * time.Second)
+	pin, err := gpio.NewRpiGPIO(4)
+	defer pin.Close()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	go blink(ctrlCh, timeoutCh, pin)
+	go timeoutGenerator(ctrlCh, timeoutCh, timeout)
+
+	fmt.Println("Press enter to quit")
+	junk := ""
+	fmt.Scanln(&junk)
+	ctrlCh <- true
+}
